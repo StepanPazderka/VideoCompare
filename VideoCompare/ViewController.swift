76c7e8cf05819754a871472d5395c611e6af6f4a
Stepan Pazderka <stepan.pazderka@icloud.com>
2020-08-23 22:39:54 +0200

    One player on the left side created

--- VideoCompare/ViewController.swift
+++ VideoCompare/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var left_videoView: UIView!
+    
+    @IBOutlet weak var right_video: UIView!
+    
+    var left_videoPlayer: AVPlayer!
+    var left_playerLayer: AVPlayerLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8")!
+//        let url = URL(string: "https://www.radiantmediaplayer.com/media/bbb-360p.mp4")!
+        left_videoPlayer = AVPlayer(url: url)
+        left_playerLayer = AVPlayerLayer(player: left_videoPlayer)
+        
+        left_playerLayer.videoGravity = .resize
+        left_videoView.layer.addSublayer(left_playerLayer)
+//        self.viewDidLayoutSubviews()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        left_videoPlayer.play()
     }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        left_playerLayer.frame = left_videoView.bounds
+    }
 
 }
 
